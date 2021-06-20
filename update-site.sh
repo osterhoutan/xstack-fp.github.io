5bcf3072ea18b59b584ec43b0b99a0d2e46d1ab3
osterhoutan-UofU <andrew.osterhout@utah.edu>
2021-06-20 16:52:22 -0600

    Updated file struct and update script

---
+++ update-site.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+set -e          # exit on failure
+set -o pipefail # pipes pass failure
+
+# << FILL ME IN TO COMPETE TEMPLATE >>
+# What is the project name (must be URI compliant)
+SITE_NAME='${ProjName}'
+# What is the URL for the Site (must be URL complient & ommit protocall)
+SITE_URL='${URL}'
+
+# Where this file should be located in the cpu-website-backend git
+SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+# The main dir of the cpu-website-backend git
+REPO_ROOT_DIR="$( cd ${SCRIPT_DIR} && cd .. && pwd )"
+# The main dir of the cpu-website git
+CONTENT_DIR="$( cd ${REPO_ROOT_DIR} && cd site-content && pwd )"
+# The location of the jekyll website directory
+SITE_SRC_DIR="$( cd ${REPO_ROOT_DIR} && cd site-src && pwd )"
+# Logfile is located in the backed git
+LOGFILE=${REPO_ROOT_DIR}/logs/update_log.txt
+# Get time stamp
+TIME_STAMP=$( date +"%Y-%m-%d %T" )
+
+#? unnessisary for hosting on github pages
+# # Location on shell that the website will be served from
+# FV_TARGET_DIR=/uusoc/sys/www/formalverification.cs.utah.edu/new
+# PAR_TARGET_DIR=/uusoc/sys/www/parallel.utah.edu
+
+#! WHILE LOOP UNNESISSARY THIS WILL BE DONE WITH A CHRON JOB INSTEAD
+# FIRST=1
+# while true
+# do
+
+# Go to the repo root and see if there are updates (via git)
+cd ${REPO_ROOT_DIR}
+CHANGED=1
+git pull |& grep -q -v 'Already up-to-date.' && CHANGED=0
+
+# if [[ ${FIRST} -eq 1 || ${CHANGED} -eq 1 ]]
+if [[ ${CHANGED} -eq 1 ]]; then
+	# There were
+	if [[ ${FIRST} -eq 1 ]]; then
+		echo -e "\nMaking sure websites are updated\n\n"
+	else
+		echo -e "\nUpdate found\n\n"
+	fi
+
+	FIRST=0
+
+	# Do a real pull to get the content
+	echo "git pull output:" |& tee -a ${LOGFILE}
+	git pull --verbose |& tee -a ${LOGFILE}
+	echo -e "\n\n" |& tee -a ${LOGFILE}
+
+	# formalverification.cs.utah.edu
+	echo -e "\nUpdating ${SITE_NAME} (${URL})\n" |& tee -a ${LOGFILE}
+
+	# Run the script that takes content and formats it for the backend
+	cd ${SCRIPT_DIR}
+	echo "update.py output:" |& tee -a ${LOGFILE}
+	./update.py ${SITE_SRC_DIR} ${CONTENT_DIR} |& tee -a ${LOGFILE}
+	echo -e "\n\n" |& tee -a ${LOGFILE}
+
+	# Update blogs
+	echo "updating blog output:" |& tee -a ${LOGFILE}
+	rm -rf ${SITE_SRC_DIR}/_posts
+	mkdir ${SITE_SRC_DIR}/_posts
+	find ${CONTENT_DIR} -regex ".*/[0-9][0-9][0-9][0-9]-[0-9]?[0-9]-[0-9]?[0-9]-.*\.md" | xargs -I{} cp -v {} ${SITE_SRC_DIR}/_posts |& tee -a ${LOGFILE}
+	echo -e "\n\n" |& tee -a ${LOGFILE}
+
+	# Run jekyll to build the new version of the website
+	cd ${SITE_SRC_DIR}
+	echo "bundle exec jekyll build output:" |& tee -a ${LOGFILE}
+	bundle exec jekyll build |& tee -a ${LOGFILE}
+	echo -e "\n\n" |& tee -a ${LOGFILE}
+
+	# Push updated repo to GitHub
+	cd ${REPO_ROOT_DIR} && git add --all && git commit -m "AUTO_BUILD :: ${TIME_STAMP}" && git push
+
+	#? unnesissary since it is hosted on github not locally
+	# # Tar it up for transfer
+	# tar zcf fv_site_tar.gz _site
+	#
+	# # Transfer it to shell
+	# echo "scp output:" |& tee -a ${LOGFILE}
+	# scp fv_site_tar.gz ${USER}@shell.cs.utah.edu:${FV_TARGET_DIR} |& tee -a ${LOGFILE}
+	# echo -e "\n\n" |& tee -a ${LOGFILE}
+	#
+	# # Ssh into shell and run the updater script
+	# echo "ssh output:" |& tee -a ${LOGFILE}
+	# ssh ${USER}@shell.cs.utah.edu "${FV_TARGET_DIR}/updateSite.bash fv_site_tar.gz"  |& tee -a ${LOGFILE}
+	# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+	echo -e "\nDone updating ${SITE_NAME} (${URL})\n\n\n" |& tee -a ${LOGFILE}
+
+fi #? added to prevent rest of commented otu code from running
+
+# # Parallel.cs.utah.edu
+# echo -e "\nUpdating parallel.cs.utah.edu\n" |& tee -a ${LOGFILE}
+
+# # Run the script that takes content and formats it for the backend
+# cd ${SCRIPT_DIR}
+# echo "update.py output:" |& tee -a ${LOGFILE}
+# ./update.py ${REPO_ROOT_DIR}/parallel ${CONTENT_DIR} -cpu |& tee -a ${LOGFILE}
+# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+# # Update blogs
+# echo "updating blog output:" |& tee -a ${LOGFILE}
+# rm -rf ${REPO_ROOT_DIR}/parallel/_posts
+# mkdir ${REPO_ROOT_DIR}/parallel/_posts
+# find ${CONTENT_DIR} -regex ".*/[0-9][0-9][0-9][0-9]-[0-9]?[0-9]-[0-9]?[0-9]-.*\.md" | xargs -I{} cp -v {} ${REPO_ROOT_DIR}/parallel/_posts |& tee -a ${LOGFILE}
+# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+# # Run jekyll to build the new version of the website
+# cd ${REPO_ROOT_DIR}/parallel
+# echo "bundle exec jekyll build output:" |& tee -a ${LOGFILE}
+# bundle exec jekyll build |& tee -a ${LOGFILE}
+# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+# # Tar it up for transfer
+# tar zcf par_site_tar.gz _site
+
+# # Transfer it to shell
+# echo "scp output:" |& tee -a ${LOGFILE}
+# scp par_site_tar.gz ${USER}@shell.cs.utah.edu:${PAR_TARGET_DIR} |& tee -a ${LOGFILE}
+# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+# # Ssh into shell and run the updater script
+# echo "ssh output:" |& tee -a ${LOGFILE}
+# ssh ${USER}@shell.cs.utah.edu "${PAR_TARGET_DIR}/updateSite.bash par_site_tar.gz" |& tee -a ${LOGFILE}
+# echo -e "\n\n" |& tee -a ${LOGFILE}
+
+# echo -e "\nDone updating parallell.cs.utah.edu\n" |& tee -a ${LOGFILE}
+
+# else
+# # No update
+# echo -n "."
+# fi
+
+# # Wait a while before trying again
+# sleep $[ 60 * 10 ]s
+# done
---
+++ update.py
@@ -0,0 +1,574 @@
+#!/usr/bin/env python3
+
+
+import re
+import sys
+import glob
+import os
+
+from os import path
+from pprint import pprint
+
+
+def error(text):
+    print(text, file=sys.stderr)
+    sys.exit(-1)
+
+
+
+field_names = [
+    "author",
+    "title",
+    "booktitle",
+    "month",
+    "year",
+    "url",
+    "crossref",
+    "type",
+    "notes",
+    "category",
+]
+
+def enter(entry, field, value):
+    field = field.strip().lower() # normalize keys
+    # replace html links with markdown links
+    value = re.sub("<a\s+href\s*=([^>]+)>([^<]+)</a>", r"[\2](\1)", value)
+    value = re.sub("and\s+and", "and", value)
+    value = value.replace("``", '"')
+    value = value.replace("''", '"')
+    value = value.strip()         # remove extra whitespace from value
+    if value[0] == "{":           # if enclosed in {} remove them
+        value = value[1:]
+    if value[-1] == ",":          # if in the middle of a list remove the ,
+        value = value[:-1]
+    if value[-1] == "}":          # if enclosed in {} remove them
+        value = value[:-1]
+    value = value.strip()         # remove extra whitespace
+    entry[field] = value
+
+
+def string_to_entry(text, origin):
+    entry = {"raw" : text,     # keep unmodified for debugging
+             "origin": origin} # also remember the user who added the file
+    lines = text.split("\n")
+
+    # look for the entry's @<at>{<entry_name>
+    at_name_match = re.match("@([\w_\-]+)\s*{\s*([\w:_\-]+)", lines[0])
+    if at_name_match == None:
+        error("unable to extract entry @ type and name from: '{}'".format(text))
+    entry["at"] = at_name_match.group(1).strip().lower()
+    entry["entry_name"] = at_name_match.group(2).strip()
+
+    curly_braces = 1
+    state = 0
+    field = ""
+    value = ""
+    for line in lines[1:]:
+        line = line.strip()
+
+        # skip blank lines
+        if line == "":
+            continue
+
+        # looking for a new {} = {}, field
+        if state == 0:
+            # there should be no partial field, value pairs floating around
+            assert field == "", field
+            assert value == "", value
+
+            # look for "="
+            try:
+                eqi = line.index("=")
+            except ValueError:
+                # None was found, something might be wrong
+                if line == "}":
+                    # the closing '}' was on its own line with the prior line
+                    #  ending in a ','
+                    state = 1
+                    continue
+                # Something is wrong
+                print("Warning: Abrubt end on raw entry:\n{}".format(text))
+                print("Stopped parsing on line:\n{}".format(line))
+                print("Data scraped:\n{}".format(entry_to_string(entry)))
+                break
+
+            # grab out field and value, note value may be incomplete
+            field = line[0:eqi]
+            value = line[eqi+1:].strip()
+            if "=" in value and "href=" not in value:
+                print("Warning: Two '=' found on one line: '{}'".format(line))
+
+            # single line values that start with '{' must end with '},'
+            if value[0] == "{" and line[-2:] == "},":
+                enter(entry, field, value)
+                field = ""
+                value = ""
+                continue
+
+            # we assume that a single line ending in a ',' contains
+            #  a complete field = value, if the value does not start with '{'
+            if value[0] != "{" and line[-1] == ",":
+                enter(entry, field, value)
+                field = ""
+                value = ""
+                continue
+
+            # we assume that a single line ending in a } contains
+            #  a complete "field = value" and is the last field in the entry
+            if line[-1] == "}":
+                state = 2
+                continue
+
+            # we got a field and a partial value, look for the rest of the value
+            value = value +  " "
+            state = 3
+            continue
+
+
+        # there should be nothing left to read in this state
+        if state == 1:
+            if line != "}":
+                print("Extra: '{}'".format(line))
+                print("Warning: Extra data at end of entry: \n{}".format(text))
+
+
+        # remember field, value and assume this was the last one
+        if state == 2:
+            enter(entry, field, value)
+            field = ""
+            value = ""
+            state = 1
+            continue
+
+
+        # have partial value looking for more
+        if state == 3:
+            # concat on the rest of the value
+            value = value + line
+
+            # we assuma a multi-line value that has '},' on the end of a line
+            #  also ends that value
+            if line[-2:] == "},":
+                enter(entry, field, value)
+                field = ""
+                value = ""
+                state = 0
+                continue
+
+            # we assuma a multi-line value that has '}' on the end of a line
+            #  also ends that value and is the last field in the entry
+            if line[-1] == "}":
+                state = 2
+                continue
+
+            # there is more to this multiline value
+            value = value.strip() +  " "
+            state = 3
+            continue
+
+    # there might be a hanging field, value pair
+    if field != "":
+        enter(entry, field, value)
+
+    if entry.get("year", "") == "":
+        entry["year"] = "unknown"
+
+    return entry
+
+
+
+
+def entry_to_string(entry):
+    string = "@{}{{{}".format(entry["at"], entry["entry_name"])
+
+    # add the fields we know in the order we decided
+    for category in field_names:
+        data = entry.get(category, None)
+        if data != None:
+            string += ",\n  {} = {{{}}}".format(category, data)
+
+    # add all other fields in sorted order
+    for field in sorted(entry):
+        if field not in field_names and field not in {"at", "entry_name"}:
+            string += ",\n  {} = {{{}}}".format(field, entry[field])
+
+    string += "\n}\n"
+
+    return string
+
+
+
+
+def read_entries(filename, origins):
+    # find the origin directory
+    rest, origin = path.split(path.dirname(filename))
+    while origin not in origins:
+        rest, origin = path.split(path.dirname(rest))
+
+    # grab all data
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    # strip comments
+    lines = [line for line in lines if not line.strip().startswith("%")]
+
+    # push together into one string
+    text = "\n" + "".join(lines)
+
+    # split on "<newline>@" (this may be brittle)
+    raw_entries = ["@"+e for e in text.split("\n@") if e.strip() != ""]
+
+    # convert to entry dicts
+    entries = [string_to_entry(e, origin) for e in raw_entries]
+
+    return entries
+
+
+
+
+def read_all_entries(filenames, origins):
+    entries = list()
+    for filename in filenames:
+        # grab all entries in each file
+        print("Reading: {}".format(filename))
+        new_entries = read_entries(filename, origins)
+
+        # check that these entries do not have the same name as existing entries
+        removable_entries = list()
+        for ne in new_entries:
+            overlap = [e for e in entries if e["entry_name"] == ne["entry_name"]]
+            if len(overlap) != 0:
+                print("Duplicate entries named '{}'".format(ne["entry_name"]))
+                removable_entries.append(ne)
+
+        for r in removable_entries:
+            new_entries.remove(r)
+
+        # add the new entries to the list of all entries
+        entries.extend(new_entries)
+
+    return entries
+
+
+
+def write_out_people(entries, filename, cpu):
+    # filter to only comment entries
+    comment_type = [e for e in entries if e["at"] == "comment"]
+    # filter those to only things marked as people or person
+    people = [e for e in comment_type if
+              ("person" in e.get("type", "").lower()
+               or "people" in e.get("type", "").lower())]
+
+    if cpu:
+        # cpu gets:
+        # - all non ganesh entres
+        # - ganesh entries only if it has the parallel type
+        query = lambda e : ("ganesh" not in  e.get("origin", "").lower()
+                            or ("ganesh" in  e.get("origin", "").lower()
+                                and "parallel" in e.get("category","").lower()))
+    else:
+        # fv gets:
+        # - only ganesh entries
+        query = lambda e : ("ganesh" in e.get("origin", "").lower())
+
+    # grab Ganesh
+    ganesh = [e for e in people if e.get("title", "") == "Ganesh Gopalakrishnan"]
+    # grab professors
+    professors = [e for e in people if
+                  (e.get("title", "") != "Ganesh Gopalakrishnan"
+                   and e.get("notes", "").lower().count("professor") >= 1
+                   and e.get("notes", "").lower().count("assistant") == 0)]
+    # grab assistant_professors
+    assistant_professors = [e for e in people if
+                            (e.get("title", "") != "Ganesh Gopalakrishnan"
+                             and e.get("notes", "").lower().count("professor") >= 1
+                             and e.get("notes", "").lower().count("assistant") >= 1)]
+    # grab researchers
+    ras = [e for e in people if
+            e.get("notes", "").lower().count("research staff") >= 1]
+    # grab PhDs
+    phds = [e for e in people if
+            e.get("notes", "").lower().count("phd student") >= 1]
+    # grab masters
+    mss = [e for e in people if
+            e.get("notes", "").lower().count("ms student") >= 1]
+    # grab alumns
+    alumns = [e for e in people if
+            e.get("notes", "").lower().count("alumni") >= 1]
+
+    # sort by name (may need to sort by last name)
+    professors.sort(key=lambda e:e["title"].split()[-1])
+    ras.sort(key=lambda e:e["title"].split()[-1])
+    phds.sort(key=lambda e:e["title"].split()[-1])
+    mss.sort(key=lambda e:e["title"].split()[-1])
+    alumns.sort(key=lambda e:e["title"].split()[-1])
+
+    # join in order
+    faculty = ganesh + professors + assistant_professors
+    students =  ras + phds + mss + alumns
+    with open(filename, "w") as f:
+        # include the frontmatter
+        f.write("---\n")
+        f.write("layout: page\n")
+        f.write("title: People\n")
+        f.write("---\n\n")
+
+        f.write("# Faculty\n")
+        for s in faculty:
+            entries.remove(s) # this entry has been consumed
+
+            # use hyperlink if present
+            if s.get("url", "") == "":
+                f.write("* {}, {}\n\n\n".format(s["title"], s["notes"]))
+            else:
+                f.write("* [{}]({}), {}\n\n\n".format(
+                    s["title"], s["url"], s["notes"]))
+
+        f.write("# Students\n")
+        for s in students:
+            entries.remove(s) # this entry has been consumed
+
+            # use hyperlink if present
+            if s.get("url", "") == "":
+                f.write("* {}, {}\n\n\n".format(s["title"], s["notes"]))
+            else:
+                f.write("* [{}]({}), {}\n\n\n".format(
+                    s["title"], s["url"], s["notes"]))
+
+
+
+
+month_to_ord = {
+    "unknown"   : 0,
+    "january"   : 1,
+    "jan"       : 1,
+    "february"  : 2,
+    "feb"       : 2,
+    "march"     : 3,
+    "mar"       : 3,
+    "april"     : 4,
+    "may"       : 5,
+    "jun"       : 6,
+    "june"      : 6,
+    "jul"       : 7,
+    "july"      : 7,
+    "august"    : 8,
+    "aug"       : 8,
+    "september" : 9,
+    "sept"      : 9,
+    "october"   : 10,
+    "oct"       : 10,
+    "november"  : 11,
+    "nov"       : 11,
+    "december"  : 12,
+    "dec"       : 12,
+}
+ord_to_month = {
+    1  : "January",
+    2  : "February",
+    3  : "March",
+    4  : "April",
+    5  : "May",
+    6  : "June",
+    7  : "July",
+    8  : "August",
+    9  : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December",
+}
+
+def write_out_publications(entries, filename, cpu):
+    # filter to only entries of type inproceedings, article, or inbook
+    publications = [e for e in entries if
+                    e["at"].lower() in {"inproceedings", "article", "inbook"}]
+
+    if cpu:
+        # cpu gets:
+        # - all non ganesh entres
+        # - ganesh entries only if it has the parallel type
+        query = lambda e : ("ganesh" not in  e.get("origin", "").lower()
+                            or ("ganesh" in  e.get("origin", "").lower()
+                                and "parallel" in e.get("category","").lower()))
+    else:
+        # fv gets:
+        # - only ganesh entries
+        query = lambda e : ("ganesh" in e.get("origin", "").lower())
+
+    to_remove = [e for e in publications if
+                 not query(e)]
+    for e in to_remove:
+        entries.remove(e)
+    publications = [e for e in publications if
+                    query(e)]
+
+    with open(filename, "w") as f:
+        for p in publications:
+            raw = p["raw"]
+            raw = re.sub("url *= *{([^}]*)}", r'url = {<a href="\1">link</a>}', raw)
+            raw = raw.replace('.pdf">link', '.pdf">pdf')
+            f.write(raw)
+            entries.remove(p)
+
+
+def write_out_education(entries, filename, cpu):
+    # filter to only comment entries
+    comment_type = [e for e in entries if e["at"] == "comment"]
+    # filter those to only education entries
+    education = [e for e in comment_type if
+                 "education" in e.get("type", "").lower()]
+    if cpu:
+        # cpu gets:
+        # - all non ganesh entres
+        # - ganesh entries only if it has the parallel type
+        query = lambda e : ("ganesh" not in  e.get("origin", "").lower()
+                            or ("ganesh" in  e.get("origin", "").lower()
+                                and "parallel" in e.get("category","").lower()))
+    else:
+        # fv gets:
+        # - only ganesh entries
+        query = lambda e : ("ganesh" in e.get("origin", "").lower())
+
+    to_remove = [e for e in education if
+                 not query(e)]
+    for e in to_remove:
+        entries.remove(e)
+    education = [e for e in education if
+                 query(e)]
+
+    # get unique years sorted from most recent to oldest
+    years = [e["year"] for e in education]
+    years = set(years)
+    years = list(years)
+    years.sort(reverse=True)
+
+    with open(filename, "w") as f:
+        # include the frontmatter
+        f.write("---\n")
+        f.write("layout: page\n")
+        f.write("title: Education/Talks\n")
+        f.write("---\n\n")
+
+        for y in years:
+            # add header for year
+            f.write("## {}\n\n".format(y))
+
+            # grab this year's entries, sort by month from most recent to oldest
+            #  entries with no month are considered the most recent
+            this_year = [e for e in education if e["year"] == y]
+            this_year.sort(key=lambda e: month_to_ord[e.get("month", "unknown").lower()],
+                           reverse=True)
+            for p in this_year:
+                entries.remove(p)  # this entry has been consumed
+                education.remove(p)  # this entry has been consumed
+
+                month_num = month_to_ord[p.get("month", "unknown").lower()]
+
+                # use hyperlink if present
+                if p.get("url", "") == "":
+                    f.write("* {}  \n".format(p["title"]))
+                else:
+                    f.write("* {} [\[LINK\]]({})  \n".format(p["title"], p["url"]))
+
+                # output author
+                f.write("   {}  \n".format(p["notes"]))
+                f.write("   {}  \n".format(p["author"]))
+
+                # output date using month if present
+                if month_num == 0:
+                    f.write("    {}\n\n".format(p["year"]))
+                else:
+                    f.write("    {}, {}\n\n".format(ord_to_month[month_num], p["year"]))
+
+
+
+
+def write_out_software(entries, filename, cpu):
+    # filter to only comment entries
+    comment_type = [e for e in entries if e["at"] == "comment"]
+    # filter those to only software entries
+    software = [e for e in comment_type if
+                "software" in e.get("type", "").lower()]
+
+    if cpu:
+        # cpu gets:
+        # - all non ganesh entres
+        # - ganesh entries only if it has the parallel type
+        query = lambda e : ("ganesh" not in  e.get("origin", "").lower()
+                            or ("ganesh" in  e.get("origin", "").lower()
+                                and "parallel" in e.get("category", "").lower()))
+    else:
+        # fv gets:
+        # - only ganesh entries
+        query = lambda e : ("ganesh" in e.get("origin", "").lower())
+
+    to_remove = [e for e in software if
+                 not query(e)]
+    for e in to_remove:
+        entries.remove(e)
+    software = [e for e in software if
+                query(e)]
+
+    # sort by name
+    software.sort(key=lambda e: e["title"])
+
+    with open(filename, "w") as f:
+        # include the frontmatter
+        f.write("---\n")
+        f.write("layout: page\n")
+        f.write("title: Software\n")
+        f.write("---\n\n")
+
+        for s in software:
+            entries.remove(s) # this entry has been consumed
+
+            # use hyperlink if present
+            if s.get("url", "") == "":
+                f.write("* {}  \n".format(s["title"]))
+            else:
+                f.write("* [{}]({})  \n".format(s["title"], s["url"]))
+
+            # output description
+            if s.get("notes", None) != None:
+                f.write("  {}\n\n".format(s["notes"]))
+
+
+
+
+
+
+
+
+def main(argv):
+    if not (len(argv) == 3 or (len(argv) == 4 and argv[3] == "-cpu")):
+        print("Usage: {} backend_git content_git [-cpu]".format(argv[0]))
+        print("  backend_git: root directory of the git with the jekyll system setup")
+        print("  content_git: root directory of the git with bib and md files to populate into the backend_git")
+        print("  -cpu: flag to filter content for the cpu website")
+        return -1
+    cpu = (len(argv) == 4)
+    backend_git = argv[1]
+    content_git = argv[2]
+
+    origins = [o for o in os.listdir(content_git) if path.isdir(path.join(content_git,o))]
+    filenames = glob.iglob(path.join(content_git, "**/*.bib"), recursive=True)
+    entries = read_all_entries(filenames, origins)
+
+    software_md = path.join(backend_git, "software.md")
+    publications_md = path.join(backend_git, "_bibliography/references.bib")# "publications.md")
+    education_md = path.join(backend_git, "education.md")
+    people_md = path.join(backend_git, "people.md")
+
+    write_out_people(entries, people_md, cpu)
+    write_out_publications(entries, publications_md, cpu)
+    write_out_education(entries, education_md, cpu)
+    write_out_software(entries, software_md, cpu)
+
+    if len(entries) != 0:
+        print("Entries with no home:")
+        for e in entries:
+            print(entry_to_string(e))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
